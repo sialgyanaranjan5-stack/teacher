c7937fce125119723a6e1d78e0a79e431818f46c
sialgyanaranjan5-stack <sialgyanaranjan5@gmail.com>
2025-09-05 00:15:32 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Function to show the special gift message
+function showMessage() {
+    document.getElementById('giftPopup').style.display = 'flex';
+}
+
+// Function to close the popup
+function closePopup() {
+    document.getElementById('giftPopup').style.display = 'none';
+}
